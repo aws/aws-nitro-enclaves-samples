34c434dade9a9d582e8d16557b6f2eb1c82a334d
Petre Eftime <epetre@amazon.com>
2022-09-27 13:45:09 +0000

    update to clap v3.2
    Signed-off-by: Petre Eftime <epetre@amazon.com>

--- vsock_sample/rs/src/main.rs
+++ vsock_sample/rs/src/main.rs
@@ -2,7 +2,6 @@ use clap::{App, AppSettings, Arg, SubCommand};
 
 use vsock_sample::command_parser::{ClientArgs, ServerArgs};
 use vsock_sample::create_app;
-use vsock_sample::utils::ExitGracefully;
 use vsock_sample::{client, server};
 
 fn main() {
@@ -10,14 +9,14 @@ fn main() {
     let args = app.get_matches();
 
     match args.subcommand() {
-        ("server", Some(args)) => {
-            let server_args = ServerArgs::new_with(args).ok_or_exit(args.usage());
-            server(server_args).ok_or_exit(args.usage());
+        Some(("server", args)) => {
+            let server_args = ServerArgs::new_with(args).unwrap();
+            server(server_args).unwrap();
         }
-        ("client", Some(args)) => {
-            let client_args = ClientArgs::new_with(args).ok_or_exit(args.usage());
-            client(client_args).ok_or_exit(args.usage());
+        Some(("client", args)) => {
+            let client_args = ClientArgs::new_with(args).unwrap();
+            client(client_args).unwrap();
         }
-        (&_, _) => {}
+        Some(_) | None => ()
     }
 }
